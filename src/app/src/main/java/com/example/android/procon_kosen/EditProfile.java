f92cd4218ce39f8712525a623a2a17837eda196f
chin8628 <58070077@kmitl.ac.th>
2016-09-05 18:46:58 +0700

    Add birthday field for EditProfile by use DatePicker

--- src/app/src/main/java/com/example/android/procon_kosen/EditProfile.java
+++ src/app/src/main/java/com/example/android/procon_kosen/EditProfile.java
@@ -1,12 +1,11 @@
 package com.example.android.procon_kosen;
 
+import android.app.FragmentManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -17,7 +16,9 @@ import java.util.Arrays;
 
 public class EditProfile extends AppCompatActivity {
 
-    Button saveBtn;
+    private Button mSaveBtn, mTestBtn;
+    private EditText mName, mSibling1, mSibling2;
+    private Spinner nBlood;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,45 +31,46 @@ public class EditProfile extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
-        EditText name = (EditText) findViewById(R.id.edit_name);
-        EditText sibling1 = (EditText) findViewById(R.id.edit_sibling_phone1);
-        EditText sibling2 = (EditText) findViewById(R.id.edit_sibling_phone2);
-        Spinner blood = (Spinner) findViewById(R.id.blood_grp_spinner);
+        mName = (EditText) findViewById(R.id.edit_name);
+        mSibling1 = (EditText) findViewById(R.id.edit_sibling_phone1);
+        mSibling2 = (EditText) findViewById(R.id.edit_sibling_phone2);
+        nBlood = (Spinner) findViewById(R.id.blood_grp_spinner);
 
         // Get value from EditText and Spinner on Edit-profile page
         SharedPreferences sharedpreferences = getSharedPreferences("contentProfle", Context.MODE_PRIVATE);
-        name.setText(sharedpreferences.getString("name", null));
-        sibling1.setText(sharedpreferences.getString("sibling1", null));
-        sibling2.setText(sharedpreferences.getString("sibling2", null));
+        mName.setText(sharedpreferences.getString("name", null));
+        mSibling1.setText(sharedpreferences.getString("sibling1", null));
+        mSibling2.setText(sharedpreferences.getString("sibling2", null));
 
         // Get value from spinner's blood group and Set selection
         String[] i = getResources().getStringArray(R.array.blood_group_spinner);
         int index_blood = Arrays.asList(i).indexOf(sharedpreferences.getString("blood", null));
-        blood.setSelection(index_blood);
+        nBlood.setSelection(index_blood);
 
-        saveBtn = (Button) findViewById(R.id.save_btn);
-        saveBtn.setOnClickListener(new View.OnClickListener() {
+        mSaveBtn = (Button) findViewById(R.id.save_btn);
+        mSaveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 SharedPreferences sharedpreferences = getSharedPreferences("contentProfle", Context.MODE_PRIVATE);
                 SharedPreferences.Editor editor = sharedpreferences.edit();
 
-                EditText name = (EditText) findViewById(R.id.edit_name);
-                EditText sibling1 = (EditText) findViewById(R.id.edit_sibling_phone1);
-                EditText sibling2 = (EditText) findViewById(R.id.edit_sibling_phone2);
-                Spinner blood = (Spinner) findViewById(R.id.blood_grp_spinner);
-
-                editor.putString("name", name.getText().toString());
-                editor.putString("blood", blood.getSelectedItem().toString());
-                editor.putString("sibling1", sibling1.getText().toString());
-                editor.putString("sibling2", sibling2.getText().toString());
+                editor.putString("name", mName.getText().toString());
+                editor.putString("blood", nBlood.getSelectedItem().toString());
+                editor.putString("sibling1", mSibling1.getText().toString());
+                editor.putString("sibling2", mSibling2.getText().toString());
                 editor.apply();
 
                 Intent i = new Intent(EditProfile.this, MainActivity.class);
                 startActivity(i);
             }
         });
+
+    }
+
+    public void showDatePickerDialog(View v) {
+        DatePickerFragment dialog = new DatePickerFragment();
+        dialog.DateEditText = (EditText) findViewById(R.id.edit_birthday);
+        dialog.show(getSupportFragmentManager(), "DialogDate");
     }
 
 }
