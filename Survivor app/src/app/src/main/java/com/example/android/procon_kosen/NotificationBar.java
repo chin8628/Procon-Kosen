05e4f8aebeab47a00f6197a73c2fb0cc82b755d1
chin8628 <boon.8628@hotmail.com>
2016-09-15 17:29:40 +0700

    Add setText method

--- Survivor app/src/app/src/main/java/com/example/android/procon_kosen/NotificationBar.java
+++ Survivor app/src/app/src/main/java/com/example/android/procon_kosen/NotificationBar.java
@@ -3,13 +3,20 @@ package com.example.android.procon_kosen;
 import android.app.Activity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class NotificationBar {
 
     private ViewGroup mViewNotificationBar;
+    private TextView mTextViewNotificationBar;
+    private String mNotification_text;
 
     public NotificationBar(Activity v) {
+        // Default notification's message is Notify_Detected (Find in string resource)
+
         mViewNotificationBar = (ViewGroup) v.findViewById(R.id.notification_bar);
+        mTextViewNotificationBar = (TextView) mViewNotificationBar.findViewById(R.id.notification_text);
+        mNotification_text = v.getResources().getString(R.string.notify_detected);
     }
     
     public void show() {
@@ -18,6 +25,11 @@ public class NotificationBar {
     
     public void hide() {
         mViewNotificationBar.setVisibility(View.INVISIBLE);
+    }
+
+    public void setText(String msg) {
+        mNotification_text = msg;
+        mTextViewNotificationBar.setText(msg);
     }
 
 }
